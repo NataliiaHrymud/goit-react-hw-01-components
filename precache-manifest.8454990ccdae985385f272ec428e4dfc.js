ec60801cbbbdec720fc080b84bbe7dcf82ecac7e
Nataliia Hrymud <ngrimud@gmail.com>
2020-07-16 10:18:16 +0300

    Updates

---
+++ precache-manifest.8454990ccdae985385f272ec428e4dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "455ff751878b9ef7913cae9a9e7515a1",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "5a8e0a5c6e15bc1289b1",
+    "url": "/goit-react-hw-01-components/static/css/main.9fa2bca7.chunk.css"
+  },
+  {
+    "revision": "f29017b74d2a9a619fdf",
+    "url": "/goit-react-hw-01-components/static/js/2.3217df56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3217df56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8e0a5c6e15bc1289b1",
+    "url": "/goit-react-hw-01-components/static/js/main.51bf8489.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
